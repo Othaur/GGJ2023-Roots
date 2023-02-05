56896acb8311e005dd2faa5f3859ea75e1de1ad0
Othaur <sleepydragongms@gmail.com>
2023-02-05 15:04:22 -0600

    CLick function

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -88,14 +88,11 @@ public class HexGrid : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            int i, j;
-            grid.GetXY(UtilsClass.GetMouseWorldPosition(), out i, out j);
-            MapGridObject currentTile = grid.GetGridObject(i, j);
-            
-            // Is the tile visible
-            if (IsTileVisible(currentTile.visualTransform.gameObject))
+            if (CanGrow(worldPos))
             {
-                // Is the tile empty
+                int i, j;
+                grid.GetXY(UtilsClass.GetMouseWorldPosition(), out i, out j);
+
                 int index = i + (j * Width);
                 if (nodes[index].State == GroundState.Empty)
                 {
@@ -133,6 +130,21 @@ public class HexGrid : MonoBehaviour
         }
     }
 
+    public bool CanGrow(Vector3 mousePosition)
+    {
+        int i, j;
+        grid.GetXY(UtilsClass.GetMouseWorldPosition(), out i, out j);
+        MapGridObject currentTile = grid.GetGridObject(i, j);
+
+        // Is the tile visible
+        if (IsTileVisible(currentTile.visualTransform.gameObject))
+        {
+            // Is the tile empty
+            return true;
+        }
+
+        return false;
+    }
     public List<Vector3Int> GetCellNeighbours(int x, int y)
     {
         return grid.GetNeighbours(x, y);
