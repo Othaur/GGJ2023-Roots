af9e4927867f5e04cb4520ad9fd830b2db22d2b5
Kanadez0s <Kanadez0s@users.noreply.github.com>
2023-02-04 19:03:11 -0600

    Fixed Perpendicular Direction

--- Assets/ExpandByClick.cs
+++ Assets/ExpandByClick.cs
@@ -102,8 +102,7 @@ public class ExpandByClick : MonoBehaviour
             updatedTriangles.Add(triangles[t]);
         }
 
-        updatedVertices.Add(target);
-        updatedUVs.Add(new Vector2(target.x , target.y));
+        
 
         int t0 = c2;
         int t1 = c;        
@@ -120,9 +119,33 @@ public class ExpandByClick : MonoBehaviour
 
         Debug.Log("Cross Product: " + crossProduct);
 
+        Vector3 mid = MidpointBetween(updatedVertices[t0], updatedVertices[t1]);
+        Vector3 dir = DirectionBetween(mid, target);
+        Vector3 t2V = target + GetPerpendicularVector(mid - target) * -Width/2f;
+        Vector3 t3V = target + GetPerpendicularVector(mid - target) * Width/2f;
+
+
+        Debug.DrawLine(target, mid, Color.yellow, 5f);
+        Debug.DrawLine(target, t2V, Color.green, 5f);
+        Debug.DrawLine(target, t3V, Color.red, 5f);
+
+
+        //updatedVertices.Add(target);
+        updatedVertices.Add(t2V);
+        updatedVertices.Add(t3V);
+
+        //updatedUVs.Add(new Vector2(target.x , target.y));
+        updatedUVs.Add(new Vector2(t2V.x, t2V.y));
+        updatedUVs.Add(new Vector2(t3V.x, t3V.y));
+
         updatedTriangles.Add(t0);
         updatedTriangles.Add(t1);
-        updatedTriangles.Add(updatedVertices.IndexOf(target));
+        //updatedTriangles.Add(updatedVertices.IndexOf(target));
+        updatedTriangles.Add(updatedVertices.IndexOf(t3V));
+
+        updatedTriangles.Add(t1);
+        updatedTriangles.Add(updatedVertices.IndexOf(t2V));
+        updatedTriangles.Add(updatedVertices.IndexOf(t3V));
         
 
 
@@ -139,5 +162,17 @@ public class ExpandByClick : MonoBehaviour
     Vector3 MidpointBetween(Vector3 a, Vector3 b)
     {
         return new Vector3(a.x + (b.x - a.x)/2f, a.y + (b.y - a.y)/2f, 0);
+    }
+
+    Vector3 DirectionBetween(Vector3 origin, Vector3 destination)
+    {
+        return (destination - origin).normalized;
+    }
+
+    Vector3 GetPerpendicularVector(Vector3 v)
+    {
+        //return CodeMonkey.Utils.UtilsClass.ApplyRotationToVector(v, -90f);
+        //return Vector2.Perpendicular(v);
+        return new Vector3(-v.y, v.x) / Mathf.Sqrt(Mathf.Pow(v.x, 2f) + Mathf.Pow(v.y, 2f));
     }
 }
